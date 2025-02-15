c88f7a3a4335147ebbb31a2c9e6930b8334f756d
RamaRagu <161216938+RamaRagu@users.noreply.github.com>
2025-02-15 10:37:42 +0530

    routes updated

--- server/src/routes/therapySessionRoutes.js
+++ server/src/routes/therapySessionRoutes.js
@@ -1,42 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const therapySessionController = require("../controllers/therapySessionController");
-const {
-  authenticateToken,
-  authorizeParent,
-  authorizeDoctor,
-} = require("../middlewares/authMiddleware");
 
 // Therapy session routes
-router.post(
-  "/",
-  authenticateToken,
-  authorizeParent,
-  therapySessionController.createSession
-);
-router.get(
-  "/:id",
-  authenticateToken,
-  authorizeParent,
-  therapySessionController.getSessionById
-);
-router.put(
-  "/:id",
-  authenticateToken,
-  authorizeParent,
-  therapySessionController.updateSessionById
-);
-router.delete(
-  "/:id",
-  authenticateToken,
-  authorizeParent,
-  therapySessionController.deleteSessionById
-);
-router.get(
-  "/doctor/sessions",
-  authenticateToken,
-  authorizeDoctor,
-  therapySessionController.getDoctorSessions
-);
+router.post("/", therapySessionController.createSession);
+router.get("/:id", therapySessionController.getSessionById);
+router.put("/:id", therapySessionController.updateSessionById);
+router.delete("/:id", therapySessionController.deleteSessionById);
+router.get("/doctor/sessions", therapySessionController.getDoctorSessions);
 
 module.exports = router;
