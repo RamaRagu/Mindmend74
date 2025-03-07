467c5378eff05f30ad91a5a812e2a2ecfc91d5d6
Rama Raguram <161216938+RamaRagu@users.noreply.github.com>
2025-03-07 15:57:36 +0000

    Updates

--- client/app/_layout.tsx
+++ client/app/_layout.tsx
@@ -1,13 +1,39 @@
-import { Stack } from 'expo-router';
-export default function RoootLayout() {
+import { Stack } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
+
+const Layout = () => {
+  const [fontsLoaded] = useFonts({
+    // Add any custom fonts here if needed
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
-      <Stack>
-        <Stack.Screen name="(tabs)"/>   
-        <Stack.Screen name="login"/> 
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="(tabs)" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="survey" />
       </Stack>
-    </Stack>
+    </View>
   );
-}
+};
+
+export default Layout;
