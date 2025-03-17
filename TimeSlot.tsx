ca7022ef0fea5cbdf032c20c8415410849ff9055
Sribaskaran Kerushan <keru18092002@gmail.com>
2025-03-17 18:39:02 +0530

    Add files via upload

---
+++ TimeSlot.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
+
+interface TimeSlotProps {
+  time: string;
+  imageUrl: string;
+}
+
+export const TimeSlot: React.FC<TimeSlotProps> = ({ time, imageUrl }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.time}>{time}</Text>
+      <View style={styles.line} />
+      <View style={styles.appointmentContainer}>
+        <Image source={{ uri: imageUrl }} style={styles.image} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+  time: {
+    color: "#051F48",
+    fontSize: 20,
+    fontWeight: "bold",
+    width: 50,
+  },
+  line: {
+    width: 55,
+    height: 1,
+    backgroundColor: "#042558",
+  },
+  appointmentContainer: {
+    width: 230,
+    height: 68,
+    borderWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 20,
+    borderRadius: 64,
+    borderColor: "#042558",
+  },
+  image: {
+    width: 60,
+    height: 56,
+    borderRadius: 30,
+  },
+});
